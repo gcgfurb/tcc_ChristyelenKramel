3c360fecb496c20f21757b4a0d6567b022ef698d
Christyelen <christyelenk@gmail.com>
2023-03-27 17:48:48 -0300

    Testes acelerometro

--- VivaParkinson/Assets/CliqueBotaoTeste.cs
+++ VivaParkinson/Assets/CliqueBotaoTeste.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class CliqueBotaoTeste : MonoBehaviour
 {
+   //public Text texto;
+
+   //public void teste(){
+    //texto = "OI";
+    //Debug.Log(texto.ToString());
+   //}
    
 }
